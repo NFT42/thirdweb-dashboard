eb505d2da380a69ab516286ccc64d85af4e4662b
Ben Tatum <mail@benjtatum.com>
2023-07-06 16:31:30 -0400

    feat: create private network

--- components/selects/NetworkSelectorButton.tsx
+++ components/selects/NetworkSelectorButton.tsx
@@ -1,7 +1,7 @@
 import { CustomChainRenderer } from "./CustomChainRenderer";
 import { popularChains } from "@3rdweb-sdk/react/components/popularChains";
-import { useColorMode } from "@chakra-ui/react";
-import { NetworkSelector, useChain, useWallet } from "@thirdweb-dev/react";
+import { useColorMode, useToast } from "@chakra-ui/react";
+import { NetworkSelector, useChain, useWallet, useChainId, useSwitchChain } from "@thirdweb-dev/react";
 import { ChainIcon } from "components/icons/ChainIcon";
 import { StoredChain } from "contexts/configured-chains";
 import { useSupportedChains } from "hooks/chains/configureChains";
@@ -9,6 +9,7 @@ import {
   useAddRecentlyUsedChainId,
   useRecentlyUsedChains,
 } from "hooks/chains/recentlyUsedChains";
+import { useModifyChain } from "hooks/chains/useModifyChain";
 import { useSetIsNetworkConfigModalOpen } from "hooks/networkConfigModal";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { BiChevronDown } from "react-icons/bi";
@@ -87,6 +88,20 @@ export const NetworkSelectorButton: React.FC<NetworkSelectorButtonProps> = ({
 
   const wallet = useWallet();
 
+  const toast = useToast();
+  const modifyChain = useModifyChain();
+  // const chainId = useChainId();
+  const switchChain = useSwitchChain();
+
+  const handleCreatePrivateNetError = () => {
+    toast({
+      title: "Failed to create private network",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
   return (
     <>
       <Button
@@ -136,8 +151,89 @@ export const NetworkSelectorButton: React.FC<NetworkSelectorButtonProps> = ({
             setShowNetworkSelector(false);
           }}
           onSwitch={(_chain) => {
+            console.log({_chain});
             onSwitchChain?.(_chain);
             addRecentlyUsedChains(_chain.chainId);
+          }}
+          onCreatePrivateNetwork={async () => {
+            let newPrivateChain: StoredChain;
+
+            try {
+              const result = await fetch("/api/stealthtest", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }).then((res) => res.json());
+              if (!result.data || result.data.status === "ERROR") {
+                return handleCreatePrivateNetError();
+              }
+              newPrivateChain = {
+                isCustom: true,
+                name: result.data.name,
+                title: result.data.name,
+                chain: result.data.chainId.toString(),
+                icon: {
+                  url: "https://app.nameless.io/favicon.svg",
+                  width: 20,
+                  height: 20,
+                  format: "svg",
+                },
+                rpc: [result.data.networks.eth.url],
+                // features?: Readonly<Array<{ name: string }>>;
+                // faucets?: readonly string[];
+                nativeCurrency: {
+                  name: "ether",
+                  symbol: "ETH",
+                  decimals: 18,
+                },
+                // infoURL?: string;
+                shortName: "ST",
+                chainId: result.data.chainId,
+                // networkId?: number;
+                // ens?: {
+                //   registry: string;
+                // };
+                // explorers?: Readonly<
+                //   Array<{
+                //     name: string;
+                //     url: string;
+                //     icon?: Icon;
+                //     standard: string;
+                //   }>
+                // >;
+                testnet: true,
+                slug: "stealthtest",
+                // slip44?: number;
+                // status?: string;
+                // redFlags?: readonly string[];
+                // parent?: {
+                //   chain: string;
+                //   type: string;
+                //   bridges?: Readonly<Array<{ url: string }>>;
+                // };
+              };
+            } catch (error) {
+              console.error(error);
+              return handleCreatePrivateNetError();
+            }
+
+            if (!newPrivateChain) {
+              return handleCreatePrivateNetError();
+            }
+
+            setShowNetworkSelector(false);
+
+            modifyChain(newPrivateChain);
+            onSwitchChain?.(newPrivateChain);
+            addRecentlyUsedChains(newPrivateChain.chainId);
+            toast({
+              title: "Private Development Network Added Successfully",
+              status: "success",
+              duration: 3000,
+              isClosable: true,
+            });
+            await switchChain(newPrivateChain.chainId);
           }}
         />
       )}
